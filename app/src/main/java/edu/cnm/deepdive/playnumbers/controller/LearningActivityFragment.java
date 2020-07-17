a37050182617fe37c9d2a9674e7031a470aba9b5
Anastasia Hristian <an.hristian@gmail.com>
2020-07-16 23:54:38 -0600

    Added images to Sign in screen.

--- app/src/main/java/edu/cnm/deepdive/playnumbers/controller/LearningActivityFragment.java
+++ app/src/main/java/edu/cnm/deepdive/playnumbers/controller/LearningActivityFragment.java
@@ -1,8 +1,42 @@
 package edu.cnm.deepdive.playnumbers.controller;
 
+import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import edu.cnm.deepdive.playnumbers.R;
+import edu.cnm.deepdive.playnumbers.model.entity.Activity.Type;
 
-public abstract class LearningActivityFragment extends Fragment {
+public class LearningActivityFragment extends Fragment {
 
-  //TODO Declare methods that can be invoked to check if LearningActivity was completed and when was completed.
+
+  public static final String TYPE_KEY = "type";
+
+  private Type type;
+
+    public static LearningActivityFragment newInstance(Type type) {
+        LearningActivityFragment fragment = new LearningActivityFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(TYPE_KEY, type);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+           this.type = (Type) args.getSerializable(TYPE_KEY);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_learning_activity, container, false);
+    }
+
 }
