406b00e18cfe69f88ae0b9b2a8ad8efc517965fd
Anastasia Hristian <an.hristian@gmail.com>
2020-08-01 13:47:34 -0600

    Working on activities.

--- app/src/main/java/edu/cnm/deepdive/playnumbers/controller/MissingNumberFragment.java
+++ app/src/main/java/edu/cnm/deepdive/playnumbers/controller/MissingNumberFragment.java
@@ -1,18 +1,38 @@
 package edu.cnm.deepdive.playnumbers.controller;
 
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.recyclerview.widget.RecyclerView;
 import edu.cnm.deepdive.playnumbers.R;
 import edu.cnm.deepdive.playnumbers.model.entity.Activity.Type;
+import java.util.Random;
+
 
 /**
  * Hosts the actions that are implemented on the activity missingNumbers.
  */
-public class MissingNumberFragment extends LearningActivityFragment {
+public class MissingNumberFragment extends LearningActivityFragment
+    implements View.OnTouchListener {
+
+  private Random rng = new Random();
+  private boolean running;
+  private boolean complete;
+  private ImageView valueDisplay;
+
+
+
+  private GestureDetectorCompat detector;
+
 
   @Nullable
   @Override
@@ -34,4 +54,41 @@ public class MissingNumberFragment extends LearningActivityFragment {
   public int getLayoutId() {
     return R.layout.fragment_missing_number;
   }
+
+  @Override
+  public boolean onTouch(View v, MotionEvent event) {
+    boolean handled = false;
+    if (running) {
+      handled = detector.onTouchEvent(event);
+      if (!handled && event.getActionMasked() == MotionEvent.ACTION_UP) {
+        valueDisplay.setTranslationX(0);
+        valueDisplay.setTranslationY(0);
+        handled = true;
+      }
+    }
+    return handled;
+  }
+
+
+
+ /* class Holder extends RecyclerView.ViewHolder {
+
+    private final ImageView imageFace;
+
+    private Holder(@NonNull ImageView itemView) {
+      super(itemView);
+      imageFace = itemView.findViewById(R.id.image_face);
+    }
+    private void bind() {
+
+      imageFace.setImageResource(R.drawable.train);
+    }
+
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+      holder.bind();
+    }
+  }*/
+
+
+
 }
